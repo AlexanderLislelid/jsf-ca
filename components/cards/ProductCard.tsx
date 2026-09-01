4c86e9dcbc05019dd96732defbb0c76a0f6ac836
AlexanderLislelid <alexlislelid@gmail.com>
2026-09-01 13:40:41 +0200

    move Product interface to its own file and use destructuring in productcard function

--- components/cards/ProductCard.tsx
+++ components/cards/ProductCard.tsx
@@ -1,40 +1,28 @@
 import Image from "next/image";
-
-export interface Product {
-  id: string;
-  title: string;
-  description: string;
-  price: number;
-  discountedPrice: number;
-  image: {
-    url: string;
-    alt: string;
-  };
-  rating: number;
-  tags: string[];
-  reviews: {
-    id: string;
-    username: string;
-    rating: number;
-    description: string;
-  }[];
-}
+import { Product } from "@/types/product";
 
 interface ProductCardProps {
   product: Product;
 }
 
-function ProductCard({ product }: ProductCardProps) {
+function ProductCard({
+  product: {
+    title,
+    image: { url, alt },
+    rating,
+    price,
+    discountedPrice,
+    description,
+  },
+}: ProductCardProps) {
   return (
     <li>
-      <h2>{product.title}</h2>
-      <Image
-        src={product.image.url}
-        alt={product.image.alt}
-        width={300}
-        height={300}
-      />
-      <p>{product.rating}</p>
+      <h2>{title}</h2>
+      <Image src={url} alt={alt} width={300} height={300} />
+      <p>{rating}</p>
+      <p>{price}</p>
+      <p>{discountedPrice}</p>
+      <p>{description}</p>
     </li>
   );
 }
