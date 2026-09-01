1439a5e0f279d97a84ad2f6df71bf412fd26e9a6
AlexanderLislelid <alexlislelid@gmail.com>
2026-09-01 18:17:57 +0200

    add price, tags, description, rating and placeholder add to cart button to ProductPage

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -9,9 +9,8 @@ async function ProductPage({ params }: { params: Promise<{ id: string }> }) {
 
   console.log(product);
   return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4">{product.title}</h1>
-
+    <div className="m-8 flex flex-col gap-6">
+      <h1 className="text-2xl font-bold">{product.title}</h1>
       {product.image && product.image.url ? (
         <Image
           src={product.image.url}
@@ -27,9 +26,33 @@ async function ProductPage({ params }: { params: Promise<{ id: string }> }) {
           height={500}
         />
       )}
+      <div>
+        {/* add logic for price display later */}
+        <p>{product.price}</p>
+        <p>{product.discountedPrice}</p>
+      </div>
+      <div className="flex gap-6">
+        {product.tags?.map((tag) => (
+          <div key={tag} className="py-1 px-3 rounded-lg bg-gray-200">
+            {tag}
+          </div>
+        ))}
+      </div>
+      <div>
+        <h3 className="text-lg font-semibold">Description:</h3>
+        <p>{product.description}</p>
+      </div>
+      <div>
+        <h3 className="text-lg font-semibold">Overall rating:</h3>
+        <p>{product.rating}/5 ☆</p>
+      </div>
+
+      {/*  note to self- create button component with add to cart functionality  */}
+
+      <button>Add to cart</button>
 
       {product.reviews.length > 0 ? (
-        <div className="my-6 mx-6">
+        <div>
           <h2 className="text-xl font-semibold mb-2">Reviews</h2>
           <ul>
             {product.reviews.map((review) => (
