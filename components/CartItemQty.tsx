c4ef00fa0019bd6ff7b12b7dea7d5c6cb13040c2
AlexanderLislelid <alexlislelid@gmail.com>
2026-09-03 15:30:06 +0200

    correct item quantity calculation

--- components/CartItemQty.tsx
+++ components/CartItemQty.tsx
@@ -3,11 +3,13 @@
 import useCartStore from "@/stores/cartStore";
 
 export default function CartItemQty() {
-  const numberOfitems = useCartStore((state) => state.items).length;
+  const numberOfItems = useCartStore((state) =>
+    state.items.reduce((sum, item) => sum + item.quantity, 0),
+  );
 
   return (
     <span className="relative right-3 bottom-1 flex h-5 w-5 items-center justify-center rounded-full bg-blue-500 text-[11px] font-bold text-white">
-      {numberOfitems}
+      {numberOfItems}
     </span>
   );
 }
