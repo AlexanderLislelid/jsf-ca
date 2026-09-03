5872d059297ed95ba7315c2c428a4b18b3a3c5f8
AlexanderLislelid <alexlislelid@gmail.com>
2026-09-03 11:44:53 +0200

    style: enhance cart page layout with flexbox and improve button icons

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import useCartStore from "@/stores/cartStore";
 import Link from "next/link";
+import { Plus, Minus } from "lucide-react";
 
 export default function CartPage() {
   const items = useCartStore((state) => state.items);
@@ -18,22 +19,33 @@ export default function CartPage() {
       ) : (
         <div>
           <h2>Shopping cart</h2>
-          <ul>
+          <ul className="flex flex-col gap-6">
             {items.map((item) => (
-              <li key={item.id}>
+              <li
+                key={item.id}
+                className="flex items-center border-2 rounded-2xl px-4 py-2"
+              >
                 <h3>{item.name}</h3>
-                <img src={item.image} alt={item.name} />
-                <div>
+                <img
+                  src={item.image}
+                  alt={item.name}
+                  height={75}
+                  width={75}
+                  className="rounded-lg"
+                />
+                <div className="flex gap-2 items-center p-2">
                   <button
                     onClick={() => updateQuantity(item.id, item.quantity + 1)}
+                    className="bg-blue-500 p-1 border border-gray-500 rounded hover:bg-blue-600"
                   >
-                    +
+                    <Plus size={"16px"} className="text-white" />
                   </button>
                   <p>{item.quantity}</p>
                   <button
                     onClick={() => updateQuantity(item.id, item.quantity - 1)}
+                    className="bg-blue-500 p-1 border border-gray-500 rounded hover:bg-blue-600"
                   >
-                    -
+                    <Minus size={"16px"} className="text-white" />
                   </button>
                 </div>
               </li>
