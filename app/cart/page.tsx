b390d7475c23e729c1c7a3a6ca8b358fd1685522
AlexanderLislelid <alexlislelid@gmail.com>
2026-09-03 11:13:58 +0200

    implement basic cart page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,3 +1,37 @@
+"use client";
+import useCartStore from "@/stores/cartStore";
+import Link from "next/link";
+
 export default function CartPage() {
-  return <div></div>;
+  const items = useCartStore((state) => state.items);
+  const removeItem = useCartStore((state) => state.removeItem);
+  const updateQuantity = useCartStore((state) => state.updateQuantity);
+  const clearAll = useCartStore((state) => state.clearCart);
+
+  return (
+    <div>
+      <h2>Shopping cart</h2>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            <h3>{item.name}</h3>
+            <img src={item.image} alt={item.name} />
+            <div>
+              <button
+                onClick={() => updateQuantity(item.id, item.quantity + 1)}
+              >
+                +
+              </button>
+              <p>{item.quantity}</p>
+              <button
+                onClick={() => updateQuantity(item.id, item.quantity - 1)}
+              >
+                -
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
