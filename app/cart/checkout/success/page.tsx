9784922256d1084d8c9914f29f34f705e6496411
AlexanderLislelid <alexlislelid@gmail.com>
2026-09-21 13:44:56 +0200

    add success page styling

--- app/cart/checkout/success/page.tsx
+++ app/cart/checkout/success/page.tsx
@@ -1,7 +1,21 @@
+import { CircleCheck } from "lucide-react";
+import Link from "next/link";
 export default function SuccessPage() {
   return (
-    <div>
-      <h1>Success</h1>
+    <div className="mt-20 p-4 bg-white rounded-md shadow-md flex flex-col">
+      <div className="flex justify-between items-center gap-4">
+        <h1 className="text-2xl font-bold">Success</h1>
+        <CircleCheck className="w-12 h-12 text-green-600" />
+      </div>
+
+      <p className="mt-4">Your order has been successfully placed.</p>
+      <p>Thank you for shopping with us!</p>
+      <Link
+        href="/"
+        className="mt-4 px-4 py-2 bg-gray-900 rounded-md text-white font-medium text-center hover:bg-black"
+      >
+        Back to Products
+      </Link>
     </div>
   );
 }
