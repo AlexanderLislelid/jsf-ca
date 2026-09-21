0facdd9552e2d564b0fd41b0c5590fecd58f92f4
AlexanderLislelid <alexlislelid@gmail.com>
2026-09-21 12:49:56 +0200

    update footer styling and add shop name

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -2,9 +2,9 @@ function Footer() {
   const year = new Date().getFullYear();
 
   return (
-    <div>
-      <p className="flex justify-center">
-        &copy; {year} ??? web shop. All rights reserved
+    <div className="py-4 bg-gray-100 border-t mt-10">
+      <p className="flex justify-center text-sm">
+        &copy; {year} Next.Aisle web shop. All rights reserved
       </p>
     </div>
   );
