90e4475c06731c872eecc102f8349f83545ed28d
AlexanderLislelid <alexlislelid@gmail.com>
2026-09-21 13:10:23 +0200

    add navigation links and logo to footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,9 +1,22 @@
+import Logo from "@/components/Logo";
+import Link from "next/link";
 function Footer() {
   const year = new Date().getFullYear();
 
   return (
-    <div className="py-4 bg-gray-100 border-t mt-10">
-      <p className="flex justify-center text-sm">
+    <div className=" bg-gray-100 border-t mt-10">
+      <div className="flex px-12 py-6 flex-col">
+        <Logo />
+        <div className="flex gap-2 flex-col mt-8">
+          <Link href="/contact" className="text-gray-600 hover:text-gray-900">
+            Contact
+          </Link>
+          <Link href="/about" className="text-gray-600 hover:text-gray-900 ">
+            About
+          </Link>
+        </div>
+      </div>
+      <p className="flex justify-center text-sm text-gray-600 mt-12">
         &copy; {year} Next.Aisle web shop. All rights reserved
       </p>
     </div>
