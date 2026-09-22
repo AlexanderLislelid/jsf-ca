7e48e52507218329756211565e554fb8e542d902
AlexanderLislelid <alexlislelid@gmail.com>
2026-09-22 11:28:03 +0200

    Add contact link to About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function AboutPage() {
   return (
     <div className="flex flex-col gap-4 mt-20 items-center max-w-2xl mx-auto px-4 text-center">
@@ -12,7 +14,11 @@ export default function AboutPage() {
         Our goal is to make online shopping fast and reliable, from searching
         for products to checking out. We are always working to improve our
         selection and service, and we'd love to hear from you if you have any
-        questions or feedback.
+        <Link href="/contact" className="text-green-700 underline">
+          {" "}
+          questions or feedback
+        </Link>
+        .
       </p>
     </div>
   );
